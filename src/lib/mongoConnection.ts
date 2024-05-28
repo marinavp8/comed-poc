4639fd806e8dc44b3b07de1fb09a17b4ebbe4327
Marina Viejo <100945399+marinavp8@users.noreply.github.com>
2024-05-28 10:38:17 +0200

    changes

--- src/lib/mongoConnection.ts
+++ src/lib/mongoConnection.ts
@@ -1,4 +1,8 @@
 import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const uri = process.env.MONGO_URL || "mongodb://localhost:27017";
 export const client = new MongoClient(uri)
 export const dbName = process.env.MONGO_DB || "comed";
